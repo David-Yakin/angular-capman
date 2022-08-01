3244ab6b8edddefa3ab45896078af44cc4ebbc9d
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-08-01 10:25:30 +0300

    making the cap-man move

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './root/app.component';
+import { PacManComponent } from './pages/pac-man/pac-man.component';
+import { MoveElementDirective } from './directives/move-element.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PacManComponent, MoveElementDirective],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
